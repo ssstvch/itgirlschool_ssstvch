97a7916765f68040fb1950cad065b635f7af7fa2
Alexandra <alexa.stvch@yandex.ru>
2021-07-16 17:56:12 +0300

    Второе задание недели

--- ex2/assets/scripts/index.js
+++ ex2/assets/scripts/index.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function add() {
+    let x = prompt(`Введите первое число:`);
+    let y = prompt(`Введите второе число:`);
+    alert(`Результат сложения: ${x} + ${y} = ${+x + +y}`);
+}
+
+function sub() {
+    let x = prompt(`Введите первое число:`);
+    let y = prompt(`Введите второе число:`);
+    alert(`Результат вычитания: ${x} - ${y} = ${+x - +y}`);
+}
+
+function multi() {
+    let x = prompt(`Введите первое число:`);
+    let y = prompt(`Введите второе число:`);
+    alert(`Результат умножения: ${x} * ${y} = ${+x * +y}`);
+}
+
+function division() {
+    let x = prompt(`Введите первое число:`);
+    let y = prompt(`Введите второе число:`);
+    alert(`Результат деления: ${x} / ${y} = ${+x / +y}`);
+
+}
