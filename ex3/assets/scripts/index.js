ac45c20a85c72938488170e4e2fbfc6ffe3787e0
Alexandra <alexa.stvch@yandex.ru>
2021-07-26 00:53:24 +0300

    Добавила 3 задание

---
+++ ex3/assets/scripts/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let slides = document.querySelectorAll('.slider_list .slider_slide');
+let currentSlide = +0;
+// let slideInterval = setInterval(nextSlide,3000);
+
+function nextSlide() {
+    slides[currentSlide].className = 'slider_slide';
+    currentSlide += 1;
+    slides[currentSlide].className = 'slider_slide showing';
+    console.log(currentSlide);
+    if (currentSlide === 4) {
+        currentSlide = 0;
+        slides[currentSlide].className = 'slider_slide'
+    }
+}
