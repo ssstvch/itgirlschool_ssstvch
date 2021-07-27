19cf77035ef1124b405c5fc99ad9049b69580d7b
Alexandra <alexa.stvch@yandex.ru>
2021-07-27 22:17:42 +0300

    Добавила первое практическое задание

--- ex1/assets/scripts/index.js
+++ ex1/assets/scripts/index.js
@@ -4,29 +4,37 @@ function result() {
     let zeroBlock = document.getElementById("zeroBlock");
     let getNum1 = document.getElementById('firstNum');
     let getNum2 = document.getElementById('secondNum');
-    console.log(getNum1.value);
     let num1 = getNum1.value;
     let num2 = getNum2.value;
     let selAct = document.getElementById('act');
     let act = selAct.options[selAct.selectedIndex].text;
-    switch (act) {
-        case '+':
-            zeroBlock.innerHTML = `Результат сложения: ${num1} + ${num2} = ${+num1 + +num2}`;
-            document.body.appendChild(zeroBlock);
-            break;
-        case '—':
-            zeroBlock.innerHTML = `Результат вычитания: ${num1} — ${num2} = ${+num1 - +num2}`;
-            document.body.appendChild(zeroBlock);
-            break;
-        case '*':
-            zeroBlock.innerHTML = `Результат умножения: ${num1} * ${num2} = ${+num1 * +num2}`;
-            document.body.appendChild(zeroBlock);
-            break;
-        case '/':
-            zeroBlock.innerHTML = `Результат деления: ${num1} / ${num2} = ${+num1 / +num2}`;
-            document.body.appendChild(zeroBlock);
-            break;
-    } 
+    if (isNaN(num1) || isNaN(num2)) {
+        alert(`Введите кореектное число!`);
+    } else {
+        switch (act) {
+            case '+':
+                zeroBlock.innerHTML = `Результат сложения: ${num1} + ${num2} = ${+num1 + +num2}`;
+                document.body.appendChild(zeroBlock);
+                break;
+            case '—':
+                zeroBlock.innerHTML = `Результат вычитания: ${num1} — ${num2} = ${+num1 - +num2}`;
+                document.body.appendChild(zeroBlock);
+                break;
+            case '*':
+                zeroBlock.innerHTML = `Результат умножения: ${num1} * ${num2} = ${+num1 * +num2}`;
+                document.body.appendChild(zeroBlock);
+                break;
+            case '/':
+                if (num2 == 0) {
+                    zeroBlock.innerHTML = `На ноль делить нельзя!`;
+                } else {
+                    zeroBlock.innerHTML = `Результат деления: ${num1} / ${num2} = ${+num1 / +num2}`;
+                    document.body.appendChild(zeroBlock);
+                }
+                break;
+        } 
+    }
+
 }
 
 
