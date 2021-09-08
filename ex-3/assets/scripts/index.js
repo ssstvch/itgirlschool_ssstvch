cba8ed835021ea99e0550718f11013bf7ad818b6
Alexandra <alexa.stvch@yandex.ru>
2021-09-09 00:10:20 +0300

    Доделала 3-е задание

--- ex-3/assets/scripts/index.js
+++ ex-3/assets/scripts/index.js
@@ -1,46 +1,82 @@
 'use strict';
 
 let form = document.querySelector(".form"),
-    username = form.querySelector(".name"),
-    img = form.querySelector(".form_img"),
-    theme = form.querySelector(".theme"),
-    text = form.querySelector(".form_text"),
+    username = form.querySelector(".form_div_input_name"),
+    img = form.querySelector(".form_div_input_img"),
+    theme = form.querySelector(".form_div_input_theme"),
+    text = form.querySelector(".form_div_input_text"),
 
-    formInput = form.querySelectorAll(".form_input"),
-    btn = form.querySelector(".form_btn"),
+    formInput = form.querySelectorAll(".form_div_input"),
+    btn = form.querySelector(".form_div_btn"),
 
     formComment = document.createElement('div'),
     userBox = document.createElement('div'),
+    imgDiv = document.createElement('div'),
+    userImage = document.createElement("img"),
     nameComment = document.createElement('div'),
+    divBtn = document.createElement("div"),
+    checkBtn = document.createElement("input"),
+    labelCheckBtn = document.createElement("label"),
     commentBox = document.createElement('div'),
     themeComment = document.createElement('div'),
     textComment = document.createElement('div'),
-    imgDiv = document.createElement('div'),
-    userImage = document.createElement("img"),
-    inputs = [],
-    image = new Image();
 
-formComment.className = "formComment";
-userBox.className = "userBox"
-nameComment.className = "form_name";
-commentBox.className = "commentBox";
-themeComment.className = "form_theme";
-textComment.className = "form_text";
-imgDiv.className = "form_imgDiv";
-image.className = "form_imgDiv_img";
+    inputs = [];
+
+    formComment.className = "form_comment";
+
+    userBox.className = "form_comment_userBox";
+    imgDiv.className = "form_comment_userBox_imgDiv";
+    userImage.className = "form_comment_userBox_imgDiv_img";
+    nameComment.className = "form_comment_userBox_name";
+    divBtn.className = "form_comment_userBox_divBtn";
+    checkBtn.className = "form_comment_userBox_divBtn_inputBox";
+    labelCheckBtn.className = "form_comment_userBox_divBtn_btn";
+    
+
+    commentBox.className = "form_comment_commentBox";
+    themeComment.className = "form_comment_commentBox_theme";
+    textComment.className = "form_comment_commentBox_text";
 
 document.addEventListener('DOMContentLoaded', () => {
     if (localStorage.length != 0) {
-        username.value = localStorage.getItem('username');
-        img.src = localStorage.getItem('photo');
-    }
+        getUserNameStorage();
+
+        username.style.display = "none";
+        img.style.display = "none";
+
+        userBox.parentElement.insertBefore(divBtn, username);
+
+        checkBtn.type = "checkbox";
+        checkBtn.name = "checkbox";
+        checkBtn.id = "checkbox";
+        divBtn.appendChild(checkBtn);
+
+        labelCheckBtn.innerHTML = "Change user";
+        labelCheckBtn.for = "name";
+        labelCheckBtn.type = "button";
+        divBtn.appendChild(labelCheckBtn);
+
+        labelCheckBtn.addEventListener('click', () => {
+            if (checkBtn.checked === false) {
+                username.style.display = "block";
+                img.style.display = "block";
+                username.style.marginTop = "20px";
+                checkBtn.checked = true;
+            } else if (checkBtn.checked === true) {
+                username.style.display = "none";
+                img.style.display = "none";
+                username.style.marginTop = "20px";
+                checkBtn.checked = false;
+            }
+        })
+        
+    } 
 })
 
 img.addEventListener('change', () => {
     let reader = new FileReader();
     let files = img.files[0];
-    
-    console.log(files.type !== "image/png" && files.type !== "image/jpg" && files.type !== "image/jpeg");
 
     if(files.type !== "image/png" && files.type !== "image/jpg" && files.type !== "image/jpeg") {
         let error = document.createElement("div");
@@ -53,7 +89,7 @@ img.addEventListener('change', () => {
         img.parentElement.firstChild.remove();
         reader.readAsDataURL(files);
         reader.onload = () => {
-            image.src = reader.result;
+        userImage.src = reader.result;
         }
     }
 
@@ -73,8 +109,6 @@ form.addEventListener('submit', (event) => {
         addComment();
         addDataToStorage();
     }
-
-    
 })
 
 const removeErrors = () => {
@@ -93,7 +127,9 @@ const checkFieldsFull = () => {
             error.classList.add("error");
             error.style.color = "red";
             error.innerHTML = `Заполните поле`;
-            console.log(formInput[i]);
+            if(formInput[i].style.display === "none") {
+                continue;
+            }
             formInput[i].parentElement.prepend(error);
         } 
     } 
@@ -109,77 +145,50 @@ const checkComment = () => {
 
 const addComment = () => {
 
-        addStyles();
+    document.body.append(formComment);
 
-        document.body.append(formComment);
+    formComment.appendChild(userBox);
+    userBox.appendChild(imgDiv);
+    if (imgDiv.firstChild) {
+        imgDiv.firstChild.remove();
+    }
+    imgDiv.appendChild(userImage);
 
-        formComment.appendChild(userBox);
-        userBox.appendChild(imgDiv);
-        imgDiv.appendChild(image);
+    userBox.appendChild(nameComment);
+    nameComment.innerHTML = `<b>User:</b> ${inputs[0]}`
 
-        userBox.appendChild(nameComment);
-        nameComment.innerHTML = `<b>User:</b> ${inputs[0]}`
+    formComment.appendChild(commentBox);
 
-        formComment.appendChild(commentBox);
+    commentBox.appendChild(themeComment)
+    themeComment.innerHTML = `<b>Theme:</b><p>${inputs[2]}</p>`
 
-        commentBox.appendChild(themeComment)
-        themeComment.innerHTML = `<b>Theme:</b><p>${inputs[2]}</p>`
-
-        commentBox.appendChild(textComment);
-        textComment.innerHTML = `<b>Comment:</b><p>${inputs[3]}</p>`
+    commentBox.appendChild(textComment);
+    textComment.innerHTML = `<b>Comment:</b><p>${inputs[3]}</p>`
 } 
-
-const addStyles = () => {
-    formComment.style.border = "1px solid rgb(197, 197, 197)";
-    formComment.style.borderRadius = "4px";
-    formComment.style.margin = "2em auto";
-    formComment.style.padding = "20px";
-    formComment.style.width = "max-content";
-    formComment.style.backgroundColor = "rgba(189, 154, 180, 0.295)";
-
-    userBox.style.display = "flex";
-    userBox.style.alignItems = "center";
-    userBox.style.padding = "8px 12px";
-    userBox.style.width = "max-content";
-    userBox.style.margin = "0 0 0 10px";
-    userBox.style.border = "1px solid rgba(197, 197, 197, 0.5)";
-    userBox.style.borderRadius = "70px";
-
-    imgDiv.style.width = "50px";
-    imgDiv.style.float = "left";
-    imgDiv.style.marginLeft = "10px";
-
-    image.style.width = "100%";
-    image.style.borderRadius = "15px 3px 3px 15px";
-
-    nameComment.style.marginLeft = "10px";
-
-    commentBox.style.margin = "0 auto";
-    commentBox.style.padding = "0 30px";
-    commentBox.style.margin = "0 auto";
-    commentBox.style.width = "max-content";
-    themeComment.style.margin = "10px auto";
-
-    textComment.style.width = "500px";
-    textComment.style.margin = "10px auto";
-    textComment.style.hyphens = "auto";
-    textComment.style.display = "table";
-
-}
 
 const addDataToStorage = () => {
     if(localStorage.length === 0) {
-        let answer = confirm(`Would you like to save your username and photo?`);
-        if (answer) {
-            localStorage.setItem('username', inputs[0]);
-            localStorage.setItem('photo', image.src);
-        }
+        localStorage.setItem('username', inputs[0]);
+        localStorage.setItem('photo', userImage.src);
     } else {
-        let answer2 = confirm(`Would you like to overwrite the username and photo`); 
-        if (answer2) {
-            localStorage.clear();
-            localStorage.setItem('username', inputs[0]);
-            localStorage.setItem('photo', image.src);
+        localStorage.clear();
+        localStorage.setItem('username', inputs[0]);
+        localStorage.setItem('photo', userImage.src);
         }
-    }
 }
+
+const getUserNameStorage = () => {
+
+    username.value = localStorage.getItem('username');
+
+    let div = username.parentElement;
+
+    div.prepend(userBox);
+    userBox.appendChild(imgDiv);
+    imgDiv.appendChild(userImage);
+    userImage.src = localStorage.getItem('photo');
+
+    userBox.appendChild(nameComment);
+    nameComment.innerHTML = `<b>User:</b> ${localStorage.getItem('username')}`;
+}
+
