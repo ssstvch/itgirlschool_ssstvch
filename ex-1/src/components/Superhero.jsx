0673c19fef29e0884bdd86d1b0d11dc13c164548
ssstvch <alexa.stvch@yandex.ru>
2021-12-14 11:19:34 +0300

    changed the structure

--- ex-1/src/components/Superhero.jsx
+++ ex-1/src/components/Superhero.jsx
@@ -1,53 +1,53 @@
 import React from 'react';
 import '../styles/superhero.scss';
 
-
 const Superhero = (props) => {
+    const {name, activity, universe, alterego, image, friends, superpower, more} = this.props;
     return (<React.Fragment>
         <article className="hero-card">
-            <h2 className="hidden">{props.name}</h2>
+            <h2 className="hidden">{name}</h2>
             <div className="hero-card__data hero-data">
-                <h2 className="hero-card__title">{props.name}</h2>
+                <h2 className="hero-card__title">{name}</h2>
 
                 <div className="hero-data__flex-container">
                     <div className="hero-data__flex-info">
 
                         <div className="hero-data__block">
                             <h3 className="hero-data__title">Деятельность:</h3>
-                            <div className="hero-data__text hero-data__text_mini">{props.activity}</div>
+                            <div className="hero-data__text hero-data__text_mini">{activity}</div>
                         </div>
                     
                         <div className="hero-data__block">
                             <h3 className="hero-data__title">Вселенная:</h3>
-                            <div className="hero-data__text hero-data__text_mini">{props.universe}</div>
+                            <div className="hero-data__text hero-data__text_mini">{universe}</div>
                         </div>
 
                         <div className="hero-data__block">
                             <h3 className="hero-data__title">Альтер Эго:</h3>
-                            <div className="hero-data__text hero-data__text_mini">{props.alterego}</div>
+                            <div className="hero-data__text hero-data__text_mini">{alterego}</div>
                         </div>
 
                     </div>    
                     
                     <div className="hero-data__imgbox">
-                        <img alt="Фото супергероя" className="hero-data__image" src={props.image}/>
+                        <img alt="Фото супергероя" className="hero-data__image" src={image}/>
                     </div>
                 </div>
 
                 <div className="hero-data__block">
                     <h3 className="hero-data__title">Друзья:</h3>
-                    <div className="hero-data__text">{props.friends}</div>
+                    <div className="hero-data__text">{friends}</div>
                 </div>
 
                 <div className="hero-data__block">    
                     <h3 className="hero-data__title">Суперспособности:</h3>
-                    <div className="hero-data__text">{props.superpower}</div>
+                    <div className="hero-data__text">{superpower}</div>
                 </div>
 
 
                 <div className="hero-data__block">
                     <h3 className="hero-data__title">Подробнее:</h3>
-                    <div className="hero-data__text">{props.more}</div>
+                    <div className="hero-data__text">{more}</div>
                 </div>
 
             </div>
